4173e98c780f77681be896c1f80d597d8b320e55
RMunroe943406 <rmunroe@sutdent.bridgew.edu>
2022-04-28 01:05:35 -0400

    added testing to the project. test correct tax rate, test type values

---
+++ tests/test_production_function.py
@@ -0,0 +1,24 @@
+import pytest
+import items
+import main
+
+
+def test_production_function_type():
+    cart = [(items.InventoryItem("generic hat a", 5.99, "clothing"))]
+    total = main.calculate_for_state(1, cart)
+    assert type(total) is float
+    assert total == 5.99
+
+
+def test_production_function_tax_rate_ma():
+    cart = [(items.InventoryItem("generic item c", 100, "everything else"))]
+    total = main.calculate_for_state(1, cart)
+    total_tax = total - cart[0].item_price
+    assert total_tax == 6.25
+
+
+def test_production_function_tax_rate_me():
+    cart = [(items.InventoryItem("generic item c", 100, "everything else"))]
+    total = main.calculate_for_state(3, cart)
+    total_tax = total - cart[0].item_price
+    assert total_tax == 5.5
