478d31656d6b601a3be9f9cbba9222f336d26225
RMunroe943406 <rmunroe@sutdent.bridgew.edu>
2022-04-28 23:31:32 -0400

    added test for clothing tax in maine, added test for empty cart

--- tests/test_production_function.py
+++ tests/test_production_function.py
@@ -22,3 +22,16 @@ def test_production_function_tax_rate_me():
     total = main.calculate_for_state(3, cart)
     total_tax = total - cart[0].item_price
     assert total_tax == 5.5
+
+
+def test_production_function_tax_rate_me_clothes():
+    cart = [(items.InventoryItem("generic item c", 100, "clothing"))]
+    total = main.calculate_for_state(3, cart)
+    total_tax = total - cart[0].item_price
+    assert total_tax == 5.5
+
+
+def test_production_function_empty_cart():
+    cart = []
+    total = main.calculate_for_state(1, cart)
+    assert total == 0
